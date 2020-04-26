4d4d584d901473722868d5e953f96cd3c9c593e4
Nirvan66 <nstp6666@gmail.com>
2020-04-26 09:54:47 -0600

    final model

--- machineLearning/geoCNN.py
+++ machineLearning/geoCNN.py
@@ -41,7 +41,7 @@ class LossAndErrorPrintingCallback(tf.keras.callbacks.Callback):
         '''
         Save model every few epochs
         '''
-        self.gGuessr.accuracy = logs['val_accuracy']
+        self.gGuessr.accuracy = round(float(logs['accuracy']),3)
         if epoch%self.saveEpoch==0:
             self.gGuessr.save(self.saveFolder)
 
@@ -63,7 +63,8 @@ def rgb(minimum, maximum, value):
 class Geoguessr:
     def __init__(self, model=None, accuracy=-1, 
                  inputShape=(300,600,3), gridCount=243,
-                 modelOptimizer=tf.keras.optimizers.Adam()):
+                 modelOptimizer=tf.keras.optimizers.Adam(), 
+                 hidden1=256, hidden2=1024):
         if model==None:
             # load restnet model
             restnet = tf.keras.applications.resnet50.ResNet50(include_top=False, 
@@ -72,19 +73,22 @@ class Geoguessr:
             self.model = tf.keras.models.Sequential()
             self.model.add(restnet)
             # freeze restnet model, will not be trained
+            # self.model.add(tf.keras.layers.GlobalAveragePooling2D())
+            # self.model.add(tf.keras.layers.Dense(gridCount, activation="softmax"))
+
+            #########################################################################
             self.model.layers[0].trainable = False
 
-            #MNIST cnn model structure to be trained
-            self.model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+            ##### MNIST cnn model structure to be trained #################################
+            self.model.add(tf.keras.layers.Conv2D(hidden1, (3, 3), activation='relu', 
+                                                    input_shape=inputShape))
             self.model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
             self.model.add(tf.keras.layers.Dropout(0.25))
             self.model.add(tf.keras.layers.Flatten())
-            self.model.add(tf.keras.layers.Dense(128, activation='relu'))
+            self.model.add(tf.keras.layers.Dense(hidden2, activation='relu'))
             self.model.add(tf.keras.layers.Dropout(0.5))
-
-            # model_finetuned.add(tf.keras.layers.GlobalAveragePooling2D())
-            # softmaxed output layer over all grids
             self.model.add(tf.keras.layers.Dense(gridCount, activation="softmax"))
+
             self.model.compile(loss=tf.keras.losses.categorical_crossentropy,
                               optimizer=modelOptimizer,
                               metrics=['accuracy'])
@@ -106,43 +110,54 @@ class Geoguessr:
             yield np.array(list(map(lambda x:np.array(load_img(dataDir+x, 
                                                                target_size=inputShape)), 
                                     X[prev:nxt]))), \
-                            tf.keras.utils.to_categorical(y[prev:nxt], num_classes=numClasses), [None]
+                            tf.keras.utils.to_categorical(y[prev:nxt], num_classes=numClasses)
             if counter>=totalBatches:
                 counter=0
+
+    def readData(self, fileNames, dataDir):
+        numClasses = self.model.layers[-1].output_shape[-1]
+        inputShape = self.model.layers[0].input_shape[1:3]
+        X = np.array(list(map(lambda x:img_to_array(load_img(dataDir+x, 
+                                                        target_size=inputShape)), 
+                      fileNames)))
+        y = np.array(list(map(lambda x: int(x.split('+')[0]),fileNames)))
+        y = tf.keras.utils.to_categorical(y, num_classes=numClasses)
+        return X,y
     
-    def fit(self, trainFiles, dataDir, saveFolder,
-            valSplit=0.97, batchSize = 10, 
-            epochs = 20, stepEpoch=5, saveEpoch=1, 
+    def fit(self, trainFiles, dataDir, saveFolder, batchSize = 10, 
+            epochs = 20, saveEpoch=1, 
             plot=False):
         # list of image file names
         # eg: <gridNo>+<lat,long>+<imageNo_date>.jpg 
         # eg: 60+48.4271513,-110.5611851+0_2009-06.jpg
-        xTrain = trainFiles[:int(len(trainFiles)*valSplit)]
-        yTrain = np.array(list(map(lambda x: int(x.split('+')[0]),xTrain)))
-        xVal = trainFiles[int(len(trainFiles)*valSplit):]
-        yVal = np.array(list(map(lambda x: int(x.split('+')[0]),xTrain)))
-        print("Getting data from directory: {}".format(dataDir))
-        print("Using {} for training and {} for validation".format(len(xTrain),len(xVal)))
-        
         callBack = [LossAndErrorPrintingCallback(self, saveFolder, saveEpoch)]
-
-        evalutaion = self.model.fit(self.dataGen(xTrain, yTrain, dataDir, batchSize=batchSize),
-                                      epochs=epochs, steps_per_epoch = stepEpoch,
-                                      callbacks=callBack, 
-                                      validation_data = self.dataGen(xVal, yVal, 
-                                                                     dataDir, batchSize=batchSize),
-                                      validation_steps = 1
-                                     )
-        self.accuracy = evalutaion.history['accuracy'][-1]
+        print("Getting data from directory: {}".format(dataDir))
+        totalBatches = len(trainFiles)/batchSize
+        counter=0
+        accuracy = []
+        loss = []
+        while(counter<=totalBatches):
+            prev = batchSize*counter
+            nxt = batchSize*(counter+1)
+            counter+=1
+            print("Reading {} points".format(len(trainFiles[prev:nxt])))
+            X,y = self.readData(trainFiles[prev:nxt], dataDir)
+            print("Read {} points".format(len(X)))
+            evalutaion = self.model.fit(X,y,
+                                          epochs=epochs, steps_per_epoch = len(X),
+                                          callbacks=callBack
+                                         )
+            accuracy += evalutaion.history['accuracy']
+            loss += evalutaion.history['loss']
 
         if plot:
-            plt.plot(evalutaion.history['val_accuracy'])
+            plt.plot(accuracy)
             plt.title('Model Accuracy')
             plt.ylabel('Accuracy')
             plt.xlabel('Epochs')
             plt.show()
 
-            plt.plot(evalutaion.history['val_accuracy'])
+            plt.plot(loss)
             plt.title('Model Loss')
             plt.ylabel('Loss')
             plt.xlabel('Epoch')
@@ -152,16 +167,17 @@ class Geoguessr:
         if self.accuracy==-1:
             print("Cannot save untrained model!!!")
         else:
-            print("\nSaving model with accuracy {} at {}".format(int(self.accuracy*100), 
+            print("\nSaving model with accuracy {} at {}".format(self.accuracy, 
                                                                saveFolder))
-            self.model.save(saveFolder + '/model_{}.h5'.format(int(self.accuracy*100)))
+            print(self.accuracy)
+            self.model.save(saveFolder + '/model_{}_.h5'.format(self.accuracy))
 
     @classmethod
     def load(cls, loadFile):
         print("Loading model from {}".format(loadFile))
         model = tf.keras.models.load_model(loadFile)
         modelFile = loadFile.split('/')[-1]
-        accuracy = int(modelFile.split('.')[0].split('_')[-1])
+        accuracy = float(modelFile.split('_')[1])
         print("Loaded model accuracy {}".format(accuracy))
         return cls(model=model, accuracy=accuracy)
     
@@ -248,16 +264,3 @@ class Geoguessr:
             embed_minimal_html('gmap.html', views=fig)
             webbrowser.open('gmap.html',new=1)
         return dist
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Train and test a seq2seq personality chatbot')
-    parser.add_argument('--trainCharachter', type=str, help='Name of character to train on. \
-                         This helps load data to train model on. eg:joey')
-    parser.add_argument('--epochs', type=int, help='Number of epochs to train on. eg:100')
-    parser.add_argument('--batchSize', type=int, help='Number of batches to split data into. eg:10')
-    parser.add_argument('--saveEpochs', type=int, help='Number of epochs to save after. eg:10')
-    parser.add_argument('--modelSaveFile', type=str, help='File to save model to. eg:joey400')
-
-    parser.add_argument('--modelLoadFile', type=str, help='File to load model from. eg:joey400')
-
-    args = parser.parse_args()
